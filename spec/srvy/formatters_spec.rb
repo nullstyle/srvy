86dcc8d697ded376b938ed3d24fc907cc992efc1
Scott Fleckenstein <nullstyle@gmail.com>
2014-02-04 11:05:24 -0800

    Adds more formatter specs

--- spec/srvy/formatters_spec.rb
+++ spec/srvy/formatters_spec.rb
@@ -15,3 +15,26 @@ describe Srvy::Formatters, "#from_name" do
     Then { expect(result).to be_kind_of(Srvy::Formatters::HostPort) }
   end
 end
+
+describe Srvy::Formatters, "#format_single" do
+  Given(:formatter) { double("Formatter", :format_single => true)}
+  Given(:name)      { "host_port" }
+  Given(:host)      { Srvy::Host.new("test01.host.com", 11211, 0, 0, 100) }
+  Given             { Srvy::Formatters.stub(:from_name){ formatter } }
+
+  When(:result) { Srvy::Formatters.format_single(name, host) }
+
+  Then{ expect(formatter).to have_received(:format_single).with(host) }
+end
+
+
+describe Srvy::Formatters, "#format_many" do
+  Given(:formatter) { double("Formatter", :format_many => true)}
+  Given(:name)      { "host_port" }
+  Given(:hosts)     { [Srvy::Host.new("test01.host.com", 11211, 0, 0, 100)] }
+  Given             { Srvy::Formatters.stub(:from_name){ formatter } }
+
+  When(:result) { Srvy::Formatters.format_many(name, hosts) }
+
+  Then{ expect(formatter).to have_received(:format_many).with(hosts) }
+end
